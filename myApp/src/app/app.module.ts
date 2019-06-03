1ce646d146644613a02eaa1f57fdbd660b3901f2
acuevas <apac.alexcuevas@gmail.com>
2019-06-03 23:45:10 +0200

    moving marker

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
+import {ErrorHandler, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundGeolocation } from '@ionic-native/background-geolocation/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +18,9 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    BackgroundGeolocation,
+    HTTP,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- myApp/src/app/tab1/tab1.page.ts
+++ myApp/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import leaflet from 'leaflet';
+import {BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse, BackgroundGeolocationEvents} from '@ionic-native/background-geolocation/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -8,8 +9,33 @@ import leaflet from 'leaflet';
 })
 export class Tab1Page {
   @ViewChild('map') mapContainer: ElementRef;
-  map: any;
-  constructor() {
+  constructor(private backgroundGeolocation: BackgroundGeolocation) {
+  }
+
+  startBackgroundGeolocation() {
+    const config: BackgroundGeolocationConfig = {
+      desiredAccuracy: 1,
+      stationaryRadius: 1,
+      distanceFilter: 1,
+      interval: 1000,
+      debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+      stopOnTerminate: false // enable this to clear background location settings when the app terminates
+    };
+
+    this.backgroundGeolocation.configure(config).then(() => {
+      this.backgroundGeolocation
+          .on(BackgroundGeolocationEvents.location)
+          .subscribe((location: BackgroundGeolocationResponse) => {
+            // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+            // and the background-task may be completed.  You must do this regardless if your operations are successful or not.
+            // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+          });
+    }).catch((err) => {
+      console.error(err);
+    });
+    // start recording location
+    this.backgroundGeolocation.start();
+
   }
 
   ionViewDidEnter() {
@@ -17,25 +43,22 @@ export class Tab1Page {
   }
 
   loadmap() {
-    this.map = leaflet.map('map').fitWorld();
+    const map = leaflet.map('map').fitWorld();
     leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
-    }).addTo(this.map);
-    this.map.locate({
+    }).addTo(map);
+    const marker = leaflet.marker(map.getCenter()).addTo(map);
+    map.locate({
       setView: true,
+      watch: true,
       maxZoom: 10
     }).on('locationfound', (e) => {
-      let markerGroup = leaflet.featureGroup();
-      let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
-        alert('Marker clicked');
-      });
-      markerGroup.addLayer(marker);
-      this.map.addLayer(markerGroup);
+      marker.setLatLng(e.latlng);
+      map.setView(marker.getLatLng(), 10);
     }).on('locationerror', (err) => {
       alert(err.message);
     });
   }
-
 
 }
