2833cab5081cc719594656401f015515a01367ae
acuevas <apac.alexcuevas@gmail.com>
2019-06-03 08:40:03 +0200

    added map

--- myApp/src/app/tab1/tab1.page.ts
+++ myApp/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import leaflet from 'leaflet';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
+  constructor() {
+  }
 
-  constructor() {}
+  ionViewDidEnter() {
+    this.loadmap();
+  }
+
+  loadmap() {
+    this.map = leaflet.map('map').fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(this.map);
+    this.map.locate({
+      setView: true,
+      maxZoom: 10
+    }).on('locationfound', (e) => {
+      let markerGroup = leaflet.featureGroup();
+      let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
+        alert('Marker clicked');
+      });
+      markerGroup.addLayer(marker);
+      this.map.addLayer(markerGroup);
+    }).on('locationerror', (err) => {
+      alert(err.message);
+    });
+  }
+
 
 }
