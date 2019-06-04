19e3efafd8720030914ab669eb05acd88c4e5c65
acuevas <apac.alexcuevas@gmail.com>
2019-06-04 16:58:30 +0200

    added polylines per run phase added countdown

--- myApp/src/app/tab1/tab1.page.ts
+++ myApp/src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import leaflet from 'leaflet';
-import {BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse, BackgroundGeolocationEvents} from '@ionic-native/background-geolocation/ngx';
+import {BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse,
+  BackgroundGeolocationEvents} from '@ionic-native/background-geolocation/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -10,12 +11,13 @@ import {BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocatio
 export class Tab1Page {
   @ViewChild('map') mapContainer: ElementRef;
   firstpolyline = new leaflet.Polyline({});
+  polylines = [];
+  count;
+  addedPoly = false;
+  map;
+  stop = false;
 
-  constructor(private backgroundGeolocation: BackgroundGeolocation) {
-    this.firstpolyline.setStyle({
-      color: 'red'
-    });
-  }
+  constructor(private backgroundGeolocation: BackgroundGeolocation) {}
 
   startBackgroundGeolocation() {
     const config: BackgroundGeolocationConfig = {
@@ -31,7 +33,9 @@ export class Tab1Page {
       this.backgroundGeolocation
           .on(BackgroundGeolocationEvents.location)
           .subscribe((location: BackgroundGeolocationResponse) => {
-            this.firstpolyline.addLatLng([location.latitude, location.longitude]);
+            if (!this.stop) {
+              this.polylines[this.polylines.length - 1].addLatLng([location.latitude, location.longitude]);
+            }
             // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
             // and the background-task may be completed.  You must do this regardless if your operations are successful or not.
             // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
@@ -47,25 +51,29 @@ export class Tab1Page {
 
   ionViewDidEnter() {
     this.loadmap();
+    this.updateCount(false, 0);
+    this.startBackgroundGeolocation();
   }
 
   loadmap() {
     let first = true;
-    const map = leaflet.map('map').fitWorld();
+    this.map = leaflet.map('map').fitWorld();
     leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
+          ' <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
-    }).addTo(map);
-    const marker = leaflet.marker(map.getCenter()).addTo(map);
-    this.firstpolyline.addTo(map);
-    map.locate({
+    }).addTo(this.map);
+    const marker = leaflet.marker(this.map.getCenter()).addTo(this.map);
+    this.firstpolyline.addTo(this.map);
+    this.polylines.push(this.firstpolyline);
+    this.map.locate({
       setView: false,
       watch: true,
       maxZoom: 10
     }).on('locationfound', (e) => {
       if (first) {
         first = false;
-        map.setView(e.latlng, 16);
+        this.map.setView(e.latlng, 16);
       }
       marker.setLatLng(e.latlng);
     }).on('locationerror', (err) => {
@@ -73,4 +81,42 @@ export class Tab1Page {
     });
   }
 
+  updateCount(down, sec) {
+    let final;
+    final = new Date();
+    final.setFullYear(0, 0, 0);
+    final.setHours(0);
+    final.setMinutes(0);
+    final.setSeconds(0);
+    if (down) {
+      final.setSeconds(final.getSeconds() + sec);
+    }
+    const count = setInterval(() => {
+      if (!this.stop) {
+        this.addedPoly = false;
+        if (down) {
+          final.setSeconds(final.getSeconds() - 1);
+        } else {
+          final.setSeconds(final.getSeconds() + 1);
+        }
+        this.count = (final.getHours() < 10 ? '0' : '') + final.getHours() + ':';
+        this.count += (final.getMinutes() < 10 ? '0' : '') + final.getMinutes() + ':';
+        this.count += (final.getSeconds() < 10 ? '0' : '') + final.getSeconds();
+        if (this.count === '00:00:00') {
+          clearInterval(count);
+        }
+      } else {
+        if (!this.addedPoly) {
+          const polyline = new leaflet.Polyline({});
+          polyline.addTo(this.map);
+          this.polylines.push(polyline);
+          this.addedPoly = true;
+        }
+      }
+    }, 1000);
+  }
+
+  pause() {
+    this.stop = !this.stop;
+  }
 }
